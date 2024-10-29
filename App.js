f84debf4835c3601a163fb377d32c621814ebda0
23048180-yongkeong <23048180@myrp.edu.sg>
2024-10-29 16:02:20 +0800

    Questions added in Exercise 2.

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Image, ScrollView} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome6';
+import RNPickerSelect from 'react-native-picker-select';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Quiz = () => {
+    const [first, setFirst] = useState('');
+    const [second, setSecond] = useState('');
+    const [third, setThird] = useState('');
+    return (
+        <View style={{margin: 10, padding: 10}}>
+          <ScrollView>
+              <Text style={{fontSize: 20, color: '#0000ff'}}>
+                <Icon name="clipboard-question" size={20} />
+            Quiz
+              </Text>
+              <Text style={{margin: 10}}>
+                  What is the background colour of this image?
+              </Text>
+              <Image source={require('./img/deer.jpg')} style={{width: 400, height: 400}} />
+              <RNPickerSelect
+                  onValueChange={(value) => console.log(value)}
+                  items={[
+                      {label: 'Red', value: 'Red'},
+                      {label: 'Green', value: 'Green'},
+                      {label: 'Blue', value: 'Blue'},
+                  ]}
+              />
+              <Text style={{margin: 10}}>
+                  What is the background colour of this image?
+              </Text>
+              <Image source={require('./img/turtle.jpg')} style={{width: 400, height: 400}} />
+              <RNPickerSelect
+                  onValueChange={(value) => console.log(value)}
+                  items={[
+                      {label: 'Blue', value: 'Blue'},
+                      {label: 'Green', value: 'Green'},
+                      {label: 'Red', value: 'Red'},
+                  ]}
+              />
+              <Text style={{margin: 10}}>
+                      What is the colour of this animal eye?
+              </Text>
+              <Image source={require('./img/owl.jpg')} style={{width: 400, height: 400}} />
+              <RNPickerSelect
+                  onValueChange={(value) => console.log(value)}
+                  items={[
+                      {label: 'Orange', value: 'Orange'},
+                      {label: 'Red', value: 'Red'},
+                      {label: 'Black', value: 'Black'},
+                  ]}
+              />
+              <Text style={{margin: 10}}>
+                  What is the colour of this animal eye?
+              </Text>
+              <Image source={require('./img/tiger.jpg')} style={{width: 400, height: 400}} />
+              <RNPickerSelect
+                  onValueChange={(value) => console.log(value)}
+                  items={[
+                      {label: 'Red', value: 'Red'},
+                      {label: 'Yellow', value: 'Yellow'},
+                      {label: 'Orange', value: 'Orange'},
+                  ]}
+              />
+          </ScrollView>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Quiz;
